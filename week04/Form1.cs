6e5fb2e80a102be0cee861ac1aed69fd58e85601
Hédi Pálinkás <palinkas.hedi@gmail.com>
2021-10-08 14:16:25 +0200

    initialize excelexport

---
+++ week04/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace week04
+{
+    public partial class Form1 : Form
+    {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+            dataGridView1.DataSource = lakasok;
+        }
+
+        public void LoadData()
+        {
+            lakasok = context.Flats.ToList();
+        
+        }
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (System.Exception ex)
+            {
+                string hiba = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(hiba, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        {
+            xlSheet.Cells[1, 1] = "Hello";
+        }
+    }
+}
