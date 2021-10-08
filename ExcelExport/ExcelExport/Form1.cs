8b080fb619593419b6eefec87a1e278daa26d2b6
Hédi Pálinkás <palinkas.hedi@gmail.com>
2021-10-08 15:25:21 +0200

    negyedik

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -16,10 +16,14 @@ namespace ExcelExport
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
         public Form1()
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
             dataGridView1.DataSource = lakasok;
         }
         public void LoadData()
@@ -27,11 +31,79 @@ namespace ExcelExport
             lakasok = context.Flats.ToList();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        public void CreateExcel()
         {
-            // TODO: This line of code loads data into the 'realEstateDataSet.Flat' table. You can move, or remove it, as needed.
-            this.flatTableAdapter.Fill(this.realEstateDataSet.Flat);
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
 
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string hiba = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(hiba, "Error");
+
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+        
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] 
+            { 
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
+            for (int i = 0; i < headers.Length; i++)
+                xlSheet.Cells[1, i+1] = headers[i];
+
+            object[,] values = new object[lakasok.Count, headers.Length];
+
+            int counter = 0;
+            foreach (var lakas in lakasok)
+            {
+                values[counter, 0] = lakas.Code;
+                counter++;
+            }
+
+            var range = xlSheet.get_Range(
+                            GetCell(0, 0),
+                            GetCell(values.GetLength(0), values.GetLength(1)));
+            range.Value2 = values;
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
     }
 }
