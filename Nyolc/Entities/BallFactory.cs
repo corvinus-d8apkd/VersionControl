3fa47daedee0a11e50b9652390b6b7f975d580e3
Hédi Pálinkás <palinkas.hedi@gmail.com>
2021-11-12 21:41:24 +0100

    absztr gyarakban

--- Nyolc/Entities/BallFactory.cs
+++ Nyolc/Entities/BallFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nyolc.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace Nyolc.Entities
 {
-    public class BallFactory
+    public class BallFactory : IToyFactory
     {
-        public Ball CreateNew()
+        public Toy CreateNew()
         {
             return new Ball();
         }
