0bec98def30db14a75ce1d275a1aaaf05b86384a
Hédi Pálinkás <palinkas.hedi@gmail.com>
2021-12-01 16:00:59 +0100

    masodik

---
+++ Szimulacio/Szimulacio/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szimulacio.Entities
+{
+    class DeathProbability
+    {
+        public Gender GenderDeath { get; set; }
+        public int AgeDeath { get; set; }
+        public double PDeath { get; set; }
+    }
+}
